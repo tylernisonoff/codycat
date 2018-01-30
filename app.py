bc43302de0a23710c54c32572c634c12a4857d66
Tyler Nisonoff <tyler@asana.com>
2018-01-29 21:36:49 -0800

    init data

--- app.py
+++ app.py
@@ -7,27 +7,29 @@ from fastai.conv_learner import *
 app = Flask(__name__, static_url_path='/static')
 print("Starting flask app...")
 
-PATH = 'data/shopstyle/'
+CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
+PATH = os.path.join(CURRENT_DIR, 'data')
 f_model = resnet34
-label_csv = f'{PATH}prod_train.csv'
-n = len(list(open(label_csv)))-1
-val_idxs = get_cv_idxs(n)
-sz = 128
+sz = 224
 
 def get_data(sz):
-    tfms = tfms_from_model(f_model, sz, aug_tfms=transforms_side_on, max_zoom=1.05)
-    return ImageClassifierData.from_csv(PATH, 'train', label_csv, tfms=tfms, suffix='.jpg', val_idxs=val_idxs, test_name='test')
+    tfms = tfms_from_model(resnet34, sz, aug_tfms=transforms_side_on, max_zoom=1.1)
+    data = ImageClassifierData.from_paths(PATH, tfms=tfms)
+    return data
 
 data = get_data(sz)
 learn = ConvLearner.pretrained(f_model, data)
 
 def load_model():
     print("Loading model...")
-    learn.load(f'{sz}')
+    learn.load('codycat_2')
     learn.precompute=False
 
 load_model()
 
+@app.route('/', methods=['GET'])
+def home():
+    return 'HOME'
 @app.route('/upload', methods=['POST'])
 def upload_file():
     print("cleaning test dir")
@@ -50,4 +52,7 @@ def upload_file():
     #throw away anything less than 0.1
     result = [item for item in tuples if item[1] > 0.1]
     #return up to 5 categories
-    return json.dumps(dict(result[:5])
+    return json.dumps(dict(result[:5]))
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
